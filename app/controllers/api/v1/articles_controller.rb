724242decc223eff3e96cf4251d9046aa7972f1a
Tanja Stroble <plusdeltah@gmail.com>
2017-11-04 22:36:12 -0700

    fixed typos so, made 1st successful postman call

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,10 @@
-module API
-  module v1
+module Api
+  module V1
     class ArticlesController < ApplicationController
 
       def index
-        @articles = Article.all.order('created_at DESC')
-        render json: {status: "SUCCESS", message: "Loaded articles", data: @articles},status: ok
+        articles = Article.order('created_at DESC')
+        render json: {status: "SUCCESS", message: "Loaded articles", data: articles},status: :ok
       end
 
     end
